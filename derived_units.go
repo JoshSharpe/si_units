ef2fc69042dc3ea89cb5a11209e5b28adffd316f
Josh Sharpe <joshasharpe@gmail.com>
2016-10-15 22:46:54 -0400

    Updated vectors and added a few more units.

--- derived_units.go
+++ derived_units.go
@@ -1,59 +1,186 @@
-package SIUnit
+package si_units
 
-type Area SIUnit
+type Area struct {
+	SIUnit
+}
 
 func NewArea(name string, area float64) *Area {
 	id := "area-" + name
 	return &Area{
-		Id:    id,
-		Name:  name,
-		Value: area,
-		Unit:  "m^2",
+		SIUnit{
+			Id:    id,
+			Name:  name,
+			Value: area,
+			Unit:  "m^2",
+		},
 	}
 }
 
 func CalculateNewArea(name string, num1 *Length, num2 *Length) *Area {
 	id := "area-" + name
 	return &Area{
-		Id:    id,
-		Name:  name,
-		Value: num1.Value * num2.Value,
-		Unit:  "m^2",
+		SIUnit{
+			Id:    id,
+			Name:  name,
+			Value: num1.Value * num2.Value,
+			Unit:  "m^2",
+		},
 	}
 }
 
-type Force SIUnit
+type Volume struct {
+	SIUnit
+}
+
+func NewVolume(name string, vol float64) *Volume {
+	id := "volume-" + name
+	return &Volume{
+		SIUnit{
+			Id:    id,
+			Name:  name,
+			Value: vol,
+			Unit:  "m^3",
+		},
+	}
+}
+
+type Density struct {
+	SIUnit
+}
+
+func NewDensity(name string, density float64) *Density {
+	id := "density-" + name
+	return &Density{
+		SIUnit{
+			Id:    id,
+			Name:  name,
+			Value: density,
+			Unit:  "kg/m^3",
+		},
+	}
+}
+
+func (d *Density) MultiplyVolume(v *Volume) *Mass {
+	return NewMass(
+		d.Name+"*"+v.Name,
+		d.Value*v.Value,
+	)
+}
+
+type FlowRate struct {
+	SIUnit
+}
+
+func NewFlowRate(name string, flowRate float64) *FlowRate {
+	id := "flowRate-" + name
+	return &FlowRate{
+		SIUnit{
+			Id:    id,
+			Name:  name,
+			Value: flowRate,
+			Unit:  "m^3/s",
+		},
+	}
+}
+
+func (f *FlowRate) MultiplyTime(t *Time) *Volume {
+	return NewVolume(
+		f.Name+"*"+t.Name,
+		f.Value*t.Value,
+	)
+}
+
+type MolarMass struct {
+	SIUnit
+}
+
+func NewMolarMass(name string, molarMass float64) *MolarMass {
+	id := "molarMass-" + name
+	return &MolarMass{
+		SIUnit{
+			Id:    id,
+			Name:  name,
+			Value: molarMass,
+			Unit:  "kg/mol",
+		},
+	}
+}
+
+// Vector units
+type Force struct {
+	SIUnit
+}
 
 func NewForce(name string, f *Vector) *Force {
 	id := "force-" + name
 	return &Force{
-		Id:          id,
-		Name:        name,
-		VectorValue: f,
-		Unit:        "N",
+		SIUnit{
+			Id:          id,
+			Name:        name,
+			VectorValue: f,
+			Unit:        "N",
+		},
 	}
 }
 
-type Velocity SIUnit
+type Displacement struct {
+	SIUnit
+}
+
+func NewDisplacement(name string, v *Vector) *Displacement {
+	id := "displacement-" + name
+	return &Displacement{
+		SIUnit{
+			Id:          id,
+			Name:        name,
+			VectorValue: v,
+			Unit:        "m",
+		},
+	}
+}
+
+type Velocity struct {
+	SIUnit
+}
 
 func NewVelocity(name string, v *Vector) *Velocity {
 	id := "velocity-" + name
 	return &Velocity{
-		Id:          id,
-		Name:        name,
-		VectorValue: v,
-		Unit:        "m/s",
+		SIUnit{
+			Id:          id,
+			Name:        name,
+			VectorValue: v,
+			Unit:        "m/s",
+		},
 	}
 }
 
-type Acceleration SIUnit
+func (v *Velocity) MultiplyTime(t *Time) *Displacement {
+	return NewDisplacement(
+		v.Name+"*"+t.Name,
+		v.VectorValue.Multiply(t.Value),
+	)
+}
+
+type Acceleration struct {
+	SIUnit
+}
 
 func NewAcceleration(name string, v *Vector) *Acceleration {
 	id := "acceleration-" + name
 	return &Acceleration{
-		Id:          id,
-		Name:        name,
-		VectorValue: v,
-		Unit:        "m/s^2",
+		SIUnit{
+			Id:          id,
+			Name:        name,
+			VectorValue: v,
+			Unit:        "m/s^2",
+		},
 	}
+}
+
+func (a *Acceleration) MultiplyTime(t *Time) *Velocity {
+	return NewVelocity(
+		a.Name+"*"+t.Name,
+		a.VectorValue.Multiply(t.Value),
+	)
 }
--- si_unit.go
+++ si_unit.go
@@ -1,4 +1,4 @@
-package SIUnit
+package si_units
 
 import "fmt"
 
@@ -11,29 +11,95 @@ type SIUnit struct {
 }
 
 func (u *SIUnit) String() string {
-	return fmt.Sprintf("%f %s", u.Value, u.Unit)
+	if u.VectorValue == nil {
+		return fmt.Sprintf("%f %s", u.Value, u.Unit)
+	}
+	return fmt.Sprintf("%f %s", u.VectorValue, u.Unit)
 }
 
-type Mass SIUnit
+func (u *SIUnit) Add(x float64) {
+	u.Value = u.Value + x
+}
+
+func (u *SIUnit) Subtract(x float64) {
+	u.Value = u.Value - x
+}
+
+func (u *SIUnit) Multiply(x float64) {
+	u.Value = u.Value * x
+}
+
+func (u *SIUnit) Divide(x float64) {
+	u.Value = u.Value / x
+}
+
+func (u *SIUnit) AddVec(vec *Vector) {
+	u.VectorValue = u.VectorValue.Add(vec)
+}
+
+func (u *SIUnit) SubtractVec(vec *Vector) {
+	u.VectorValue = u.VectorValue.Subtraction(vec)
+}
+
+func (u *SIUnit) MultiplyVec(scalar float64) {
+	u.VectorValue = u.VectorValue.Multiply(scalar)
+}
+
+func (u *SIUnit) DivideVec(scalar float64) {
+	u.VectorValue = u.VectorValue.Divide(scalar)
+}
+
+type Mass struct {
+	SIUnit
+}
 
 func NewMass(name string, mass float64) *Mass {
 	id := "mass-" + name
 	return &Mass{
-		Id:    id,
-		Name:  name,
-		Value: mass,
-		Unit:  "kg",
+		SIUnit{
+			Id:    id,
+			Name:  name,
+			Value: mass,
+			Unit:  "kg",
+		},
 	}
 }
 
-type Length SIUnit
+func (m *Mass) MultiplyAcceleration(a *Acceleration) *Force {
+	return NewForce(
+		m.Name+"*"+a.Name,
+		a.VectorValue.Multiply(m.Value),
+	)
+}
+
+type Length struct {
+	SIUnit
+}
 
 func NewLength(name string, length float64) *Length {
 	id := "length-" + name
 	return &Length{
-		Id:    id,
-		Name:  name,
-		Value: length,
-		Unit:  "m",
+		SIUnit{
+			Id:    id,
+			Name:  name,
+			Value: length,
+			Unit:  "m",
+		},
+	}
+}
+
+type Time struct {
+	SIUnit
+}
+
+func NewTime(name string, time float64) *Time {
+	id := "time-" + name
+	return &Time{
+		SIUnit{
+			Id:    id,
+			Name:  name,
+			Value: time,
+			Unit:  "s",
+		},
 	}
 }
